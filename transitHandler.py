f0475de80fd0415817a571426a98625f1f342a87
liamocuz <liamo@me.com>
2022-07-23 12:40:33 -0400

    Turned into an abstract class for loading info from text files

---
+++ transitHandler.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+import pandas
+
+
+
+class TransitHandler(ABC):
+    """Parent class for importing information from NJ Transit text files"""
+    def __init__(self, path: str):
+        self.dataframe = self.getDataframe(path)
+
+    @staticmethod
+    def getDataframe(path: str):
+        dataframe = None
+        try:
+            dataframe = pandas.read_csv(path, delimiter=',')
+        except Exception as e:
+            print(f"Unable to read {path}")
+            print(e)
+            raise IOError
+
+        return dataframe
+
+    @abstractmethod
+    def buildDictionary(self):
+        pass
