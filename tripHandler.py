6cc9c06ba0e82d9d5acafe9bb7e82d9eac4b20cc
liamocuz <liamo@me.com>
2022-07-23 14:37:57 -0400

    Added buildDictionary method

---
+++ tripHandler.py
@@ -0,0 +1,33 @@
+from transitHandler import TransitHandler
+from dataclasses import dataclass
+
+
+@dataclass
+class Trip:
+    """Class for representing a rail trip"""
+    route_id: int
+    service_id: int
+    trip_id: int
+    trip_headsign: str
+    direction_id: int
+    block_id: str
+    shape_id: int
+
+
+class TripHandler(TransitHandler):
+    """Class for loading in data from ./rail-data/trips.txt"""
+    def __init__(self, path: str):
+        super().__init__(path)
+        self.dictionary = self.buildDictionary()
+
+    def buildDictionary(self) -> dict:
+        dictionary = dict()
+        ROUTE_ID_INDEX = 0
+        TRIP_ID_INDEX = 2
+
+        for row in self.dataframe.values:
+            new_trip = Trip(*row)
+            dictionary[row[ROUTE_ID_INDEX]] = new_trip
+            dictionary[row[TRIP_ID_INDEX]] = new_trip
+
+        return dictionary
