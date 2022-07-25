089d51961fc526a7574a4022e3c84068ec9036bf
liamocuz <liamo@me.com>
2022-07-25 12:42:11 -0400

    Created new transit handler to control data

---
+++ transit_handler.py
@@ -0,0 +1,11 @@
+"""
+This file creates methods to retrieve info from the Handler classes
+Given a station, find out the next arrivals
+"""
+
+
+class TransitHandler:
+    """This class handles getting information from all data sources to construct routes and stop info"""
+
+    def __init__(self):
+        pass
