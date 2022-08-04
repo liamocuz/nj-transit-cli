be249df02549845a10e3bcd03fec36fe649474b0
liamocuz <liamo@me.com>
2022-08-04 17:04:14 -0500

    Changed around some input args

--- njt.py
+++ njt.py
@@ -10,7 +10,7 @@ import zipfile
 import argparse
 import requests
 from transit_handler import TransitHandler
-from dates_and_times import get_today_date, get_tomorrow_date
+from dates_and_times import get_today_date
 
 
 def get_rail_data(url: str, zip_path: str, extract_path: str) -> bool:
@@ -50,29 +50,47 @@ if __name__ == "__main__":
 
     # Create the argument parser and add arguments
     parser = argparse.ArgumentParser(description="print out departure times for a rail stop")
-    parser.add_argument("stop_name",
-                        metavar="stop_name",
-                        type=str,
-                        help="name of the rail stop. surround the name in quotes if it has spaces")
-    parser.add_argument("-d",
-                        "--date",
-                        default=get_today_date(),
-                        nargs='?',
-                        help="the date of the departure times in a YYYYMMDD format")
-    parser.add_argument("-r",
-                        "--refresh",
-                        action="store_true",
-                        help="force a download the NJ Transit Rail Data")
-    parser.add_argument("-t",
-                        "--tomorrow",
-                        action="store_true",
-                        help="gets times for tomorrow. this will override any date specified by -d")
-    parser.add_argument("-l",
-                        "--list",
-                        nargs='?',
-                        type=int,
-                        default=float("inf"),
-                        help="number of times to list per headsign")
+    parser.add_argument(
+        "names",
+        metavar="names",
+        type=str,
+        nargs='+',
+        help="name of the rail stop. surround the name in quotes if it has spaces"
+    )
+    parser.add_argument(
+        "-d",
+        "--date",
+        type=str,
+        default=get_today_date(),
+        nargs='?',
+        help="the date of the departure times in a YYYY-MM-DD format. "
+             "default is today's date"
+    )
+    parser.add_argument(
+        "-r",
+        "--refresh",
+        action="store_true",
+        help="force a download the NJ Transit Rail Data"
+    )
+    parser.add_argument(
+        "-t",
+        "--time",
+        type=str,
+        default="00:00",
+        nargs='?',
+        help="specify a minimum departure time in a 24hr format HH:MM. "
+             "default is 12 AM (00:00)"
+    )
+    parser.add_argument(
+        "-n",
+        "--number",
+        type=int,
+        nargs='?',
+        default=10,
+        help="number of times to list per headsign. "
+             "default is 10 times after the specified time"
+    )
+    # might need some more args
     args = parser.parse_args()
 
     # Handle downloading and unzipping the rail data
@@ -83,11 +101,16 @@ if __name__ == "__main__":
             sys.exit(1)
 
     # Create the transit handler, which is the main handler for retrieving data
-    transit = TransitHandler()
-    transit.get_station_info(
-        name=str(args.stop_name),
-        date=get_tomorrow_date() if args.tomorrow else str(args.date),
-        list_length=args.list
-    )
+    try:
+        transit = TransitHandler(date=str(args.date), time=str(args.time))
+    except ValueError as exc:
+        print(exc)
+        sys.exit(0)
+
+    for name in args.names:
+        transit.get_station_info(
+            name=str(name),
+            list_length=int(args.number)
+        )
 
     sys.exit(0)
